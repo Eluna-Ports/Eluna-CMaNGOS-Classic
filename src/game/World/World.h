0b35a8ae35c74bbf35f6e5bfab2de19746110eec
Foereaper <foereaper@elunatech.com>
2024-01-28 03:46:46 +0100

    Add Eluna multistate support
    Missing async query support, needs to be researched
    
    Currently untested other than it compiles

--- src/game/World/World.h
+++ src/game/World/World.h
@@ -41,6 +41,9 @@
 #include <vector>
 #include <array>
 
+#ifdef BUILD_ELUNA
+class Eluna;
+#endif
 class Object;
 class ObjectGuid;
 class WorldPacket;
@@ -415,9 +418,6 @@ enum eConfigBoolValues
     CONFIG_BOOL_PET_ATTACK_FROM_BEHIND,
     CONFIG_BOOL_AUTO_DOWNRANK,
     CONFIG_BOOL_MMAP_ENABLED,
-#ifdef BUILD_ELUNA
-    CONFIG_BOOL_ELUNA_ENABLED,
-#endif
 #ifdef BUILD_SOLOCRAFT
     CONFIG_BOOL_SOLOCRAFT_ENABLED,
     CONFIG_BOOL_SOLOCRAFT_ANNOUNCE,
@@ -713,6 +713,11 @@ class World
 
         LFGQueue& GetLFGQueue() { return m_lfgQueue; }
         void StartLFGQueueThread();
+
+#ifdef BUILD_ELUNA
+        Eluna* GetEluna() const { return eluna; }
+        Eluna* eluna;
+#endif
     protected:
         void _UpdateGameTime();
         // callback for UpdateRealmCharacters
